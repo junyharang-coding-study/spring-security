e5ac0e2b4788118fce95b5f46578970a014477dd
홍세준 <juny3170@secuchart.com>
2022-06-14 21:18:19 +0900

    First Commit

---
+++ src/main/java/com/junyharang/spring/security/JunyharangSpringSecutiryApplication.java
@@ -0,0 +1,13 @@
+package com.junyharang.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunyharangSpringSecutiryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunyharangSpringSecutiryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/junyharang/spring/security/JunyharangSpringSecutiryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junyharang.spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunyharangSpringSecutiryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
