2e0e85974ae74d2a7310949096c744b52ccea945
주니하랑 <junyharang8592@gmail.com>
2022-08-09 22:44:26 +0900

    feat : [SecutiryController] 4개의 임시 Get Method 설정 및 각각 요청이 들어왔을 때, 요청 이용자 ID와 권한 정보를 볼 수 있게 처리.
    Signed-off-by: 주니하랑 <junyharang8592@gmail.com>

--- src/main/java/com/junyharang/spring/security/SecurityController.java
+++ src/main/java/com/junyharang/spring/security/SecurityController.java
@@ -1,16 +1,57 @@
 package com.junyharang.spring.security;
 
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController public class SecurityController {
+import java.security.Principal;
+import java.util.Collection;
 
-    @GetMapping("/") public String index() {
-        return "Hello, JunyHarang 👋";
+@RestController
+public class SecurityController {
+
+    @GetMapping("/")
+    public String index(Principal principal) {
+        return "Hello, JunyHarang 👋 This is JunyHarang's Home <br><br> 요청 이용자 ID 정보 : " + principal.getName() + " <br> 요청 이용자 권한 정보 : " + getRoleInfo();
     } // String index() 끝
 
-    @GetMapping("/signin") public String signIn() {
+    @GetMapping("/signin")
+    public String signIn() {
         return "이 곳은 Login Page 입니다!";
     } // signIn() 끝
+
+    @GetMapping("/user")
+    public String user(Principal principal) {
+        return "This is JunyHarang's user Page <br><br> 요청 이용자 ID 정보 : " + principal.getName() + " <br> 요청 이용자 권한 정보 : " + getRoleInfo();
+    } // user() 끝
+
+    @GetMapping("/admin/pay")
+    public String adminPay(Principal principal) {
+        return "This is JunyHarang's pay Page <br><br> 요청 이용자 ID 정보 : " + principal.getName() + " <br> 요청 이용자 권한 정보 : " + getRoleInfo();
+    } // adminPay() 끝
+
+    @GetMapping("/admin/test")
+    public String adminTest(Principal principal) {
+        return "This is JunyHarang's admin Test Page <br><br> 요청 이용자 ID 정보 : " + principal.getName() + " <br> 요청 이용자 권한 정보 : " + getRoleInfo();
+    } // adminTest() 끝
+
+    @GetMapping("/admin/management")
+    public String adminManagement(Principal principal) {
+        return "This is JunyHarang's admin Management Page <br><br> 요청 이용자 ID 정보 : " + principal.getName() + " <br> 요청 이용자 권한 정보 : " + getRoleInfo();
+    } // adminManagement() 끝
+
+    /**
+     * 이용자 권한 반환 Method
+     *
+     * @return Collection<? extends GrantedAuthority> - 요청 이용자 보유 권한 목록
+     */
+
+    private Collection<? extends GrantedAuthority> getRoleInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        return authentication.getAuthorities();
+    } // getRoleInfo() 끝
 } // class 끝
