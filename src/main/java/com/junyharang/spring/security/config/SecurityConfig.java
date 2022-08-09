203c19746719f0a7c272eb277ac3fc7289b5c1a5
주니하랑 <junyharang8592@gmail.com>
2022-08-09 22:43:12 +0900

    feat : [SecutiryConfig] Memory 상에 임시 계정 생성 설정 및 인가 처리 기능 구현
    Signed-off-by: 주니하랑 <junyharang8592@gmail.com>

--- src/main/java/com/junyharang/spring/security/config/SecurityConfig.java
+++ src/main/java/com/junyharang/spring/security/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.junyharang.spring.security.config;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -36,6 +37,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
 
     /**
+     * 이용자 생성 및 권한 설정
+     *
+     * @param auth - 세부 인가 처리 기능 설정 API 제공 객체
+     */
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        /* Memory 내에 임시 이용자 추가 */
+        auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
+        auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS", "USER");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN", "SYS", "USER");
+    }
+
+    /**
      * Spring Security 설정 Method
      *
      * @param http - 세부 보안 기능 설정 API 제공 객체
@@ -44,17 +59,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()                                    // Client가 http 방식으로 요청을 보내면
-                .anyRequest().authenticated()                           // 모든 요청에 대해 인증 검사를 수행한다.
-                .and()                                                  // 그리고,
-                .formLogin()                                            // 인증 방식은 formLogin 방식
-//                  .loginPage("/signin")                               // 개발자 정의 Login Page 설정
-                .defaultSuccessUrl("/")                                 // Login 성공 시 이동할 위치
-                .failureUrl("/signin")               // Login 실패 시 이동할 위치
-                .usernameParameter("userId")                            // 회원 ID를 입력 받을 변수 이름 정의 (기본은 username이나, 변경 가능)
-                .passwordParameter("password")                          // 회원 비밀번호 입력 받을 변수 이름 정의 (기본은 password이나, 변경 가능)
-                .loginProcessingUrl("/signin/proc")                     // Login Form Action URL (기본은 "/login" 이며, 강의에서는 "/login_proc" 으로 설정)
-                .successHandler(new AuthenticationSuccessHandler() {    // Login 성공 시 호출 될 Handler 정의
+                /* 인가 처리 */
+                .authorizeRequests()                                                                                // Client가 http 방식으로 요청을 보내면
+                .antMatchers("/user").hasRole("USER")                                                   // 요청 이용자가 /user URI에 대한 요청을 하였을 때, 해당 URI 권한 중 USER 권한으로 이용자가 접근하였는지 확인.
+                .antMatchers("/admin/pay").hasRole("ADMIN")                                             // 요청 이용자가 /admin/pay URI에 대한 요청을 하였을 때, 해당 URI 권한 중 ADMIN 권한으로 이용자가 접근하였는지 확인.
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")          // 요청 이용자가 /admin 하위 URI에 대한 요청을 하였을 때, 해당 URI 권한 중 ADMIN과 SYS 권한으로 이용자가 접근하였는지 확인.
+                .anyRequest().authenticated()                                                                       // 모든 요청에 대해 인증 검사를 수행한다.
+                .and()                                                                                              // 그리고,
+                .formLogin()                                                                                        // 인증 방식은 formLogin 방식
+//              .loginPage("/signin")                                                                               // 개발자 정의 Login Page 설정
+                .defaultSuccessUrl("/")                                                                             // Login 성공 시 이동할 위치
+                .failureUrl("/signin")                                                           // Login 실패 시 이동할 위치
+                .usernameParameter("userId")                                                                        // 회원 ID를 입력 받을 변수 이름 정의 (기본은 username이나, 변경 가능)
+                .passwordParameter("password")                                                                      // 회원 비밀번호 입력 받을 변수 이름 정의 (기본은 password이나, 변경 가능)
+                .loginProcessingUrl("/signin/proc")                                                                 // Login Form Action URL (기본은 "/login" 이며, 강의에서는 "/login_proc" 으로 설정)
+                .successHandler(new AuthenticationSuccessHandler() {                                                // Login 성공 시 호출 될 Handler 정의
                     @Override
                     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
@@ -117,8 +136,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .sessionManagement()
                 .maximumSessions(1)                     // 최대 Session 허용 개수 1개
-//              .maxSessionsPreventsLogin(true);        // 현재 이용자 인증을 실패하도록 하는 전략
-                .maxSessionsPreventsLogin(false);       // 이전 이용자 Session을 만료 시키는 전략 - Default Value
+                .maxSessionsPreventsLogin(true);        // 현재 이용자 인증을 실패하도록 하는 전략
+//                .maxSessionsPreventsLogin(false);       // 이전 이용자 Session을 만료 시키는 전략 - Default Value
 
                 /* Session 고정 보호 기능 활성화 */
         http
